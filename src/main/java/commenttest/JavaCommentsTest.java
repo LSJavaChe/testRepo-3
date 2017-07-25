72cacc81ec9e9d89943edfe855b8202cb169ba3b
idexmai <uservar2mw@some.mail>
2017-07-25 17:05:09 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1500993784095
+//first_change1500997910987
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
