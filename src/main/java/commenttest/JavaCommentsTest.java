cf5ef8aed15ef07787d3a08b460622a24d4f4f73
LSJavaChe <che@eclipse.org>
2018-02-14 22:39:48 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1518555552705
+//first_change1518644847350
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
